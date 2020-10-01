47dc2ce1b4d69cbf8304363ff5c9d93c5a3eb54f
Kevin Candelaria <kevdashdev@gmail.com>
2020-10-01 21:16:41 +0800

    Add gitignore

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/kevincandelaria/labs/learning/rust/learning-rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/kevincandelaria/labs/learning/rust/learning-rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/kevincandelaria/labs/learning/rust/learning-rust/hello_cargo/target/debug/hello_cargo: /Users/kevincandelaria/labs/learning/rust/learning-rust/hello_cargo/src/main.rs
