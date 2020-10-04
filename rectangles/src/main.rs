e3130880111ab34f7bd280a9a765a7c95980b662
Kevin Candelaria <kevdashdev@gmail.com>
2020-10-04 15:10:48 +0800

    Methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,23 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.area() >= rectangle.area()
+    }
+}
+
 fn main() {
     let width = 30;
     let height = 50;
@@ -14,14 +31,27 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(&rectangle)
+        rectangle.area()
     );
 
     println!("width is {}", rectangle.width);
     println!("height is {}", rectangle.height);
     println!("rectangle is {:#?}", rectangle);
+
+    let rectangle2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+
+    let rectangle3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    let square = Rectangle::square(30);
+
+    println!("Rectangle#1 can hold Rectangle#2: {}", rectangle.can_hold(&rectangle2));
+    println!("Rectangle#1 can hold Rectangle#3: {}", rectangle.can_hold(&rectangle3));
+    println!("Rectangle#1 can hold Square: {}", rectangle.can_hold(&square));
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
