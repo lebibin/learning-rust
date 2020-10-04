6ad80deb653a13f938510f3e7bca8538845fdae7
Kevin Candelaria <kevdashdev@gmail.com>
2020-10-04 14:54:17 +0800

    Refactor with struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,13 +1,27 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
 fn main() {
     let width = 30;
     let height = 50;
+    let rectangle = Rectangle {
+        width,
+        height
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area((width, height))
+        area(&rectangle)
     );
+
+    println!("width is {}", rectangle.width);
+    println!("height is {}", rectangle.height);
+    println!("rectangle is {:#?}", rectangle);
 }
 
-fn area(dimension: (u32, u32)) -> u32 {
-    dimension.0 * dimension.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
